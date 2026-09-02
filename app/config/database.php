2e20b388f2ce97b9ce6a296c9004e7e40a499e4a
clarkhidalgo15-maker <clarkhidalgo15@gmail.com>
2026-09-02 21:05:01 +0800

    Fix database config syntax

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> getenv('DB_DRIVER') ?: 'mysql',
-    'hostname'\t=> getenv('DB_HOST') ?: 'clark-clarkhidalgo15-d1d6.h.aivencloud.com',
-    'port'\t\t=> getenv('DB_PORT') ?: '13756',
-    'username'\t=> getenv('DB_USER') ?: 'avnadmin',
+    'hostname' => getenv('DB_HOST') ?: 'clark-clarkhidalgo15-d1d6.h.aivencloud.com',
+    'port' => getenv('DB_PORT') ?: '13756',
+    'username' => getenv('DB_USER') ?: 'avnadmin',
     'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'\t=> getenv('DB_NAME') ?: 'defaultdb',
+    'database' => getenv('DB_NAME') ?: 'defaultdb',
     'charset'	=> getenv('DB_CHARSET') ?: 'utf8mb4',
     'dbprefix'	=> getenv('DB_PREFIX') ?: '',
     // Optional for SQLite
